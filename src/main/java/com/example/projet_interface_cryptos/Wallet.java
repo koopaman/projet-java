dee54d07c051830e556effca1e40946f3dd246de
koopaman <pedron.arthur2003@gmail.com>
2024-01-10 19:23:38 +0100

    test

--- src/main/java/com/example/projet_interface_cryptos/Wallet.java
+++ src/main/java/com/example/projet_interface_cryptos/Wallet.java
@@ -16,5 +16,7 @@ public class Wallet extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        gzergzegzerg
+                hre-hsrh
     }
 }
