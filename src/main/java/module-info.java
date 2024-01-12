ba5b703fd2125b702afc67dc15ad0c04c1cfef54
alexisssmrt <alexis.martin@eleve.isep.fr>
2024-01-12 16:26:10 +0100

    début du code pour les API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.projet_interface_cryptos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens com.example.projet_interface_cryptos to javafx.fxml;
